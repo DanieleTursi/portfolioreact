cc0b462523d5bccc6974dd95487a29ca8f538c79
Daniele Tursi <daniele@Danieles-MacBook-Air.local>
2023-07-13 21:26:35 +0100

    first commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components"
+
+const Home=()=>{
+
+
+    return(
+      <MainWrap>
+      <h1>Hellooooo</h1>
+      </MainWrap>
+    )
+}
+
+export default Home;
+
+const MainWrap=styled.div`
+   width:100vw;
+   height:100vh;
+   background:black;
+   h1{
+    margin:0;
+   }
+`
